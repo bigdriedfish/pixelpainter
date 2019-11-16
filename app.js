e039481e8d6eb7da3d227b0c78e523a839d8c8ea
xiaoyugan <1690439769@qq.com>
2019-11-16 19:40:38 +0800

    基本功能

--- app.js
+++ app.js
@@ -9,21 +9,33 @@ const server = app.listen(port, () => {
 	console.log('server listening on port', port)
 })
 
-const io = sockerIO(server)
+const io = socketIO(server)
 
 //app.use(express.static(path, join(__dirname, './dist')))
 
 const pixelData = [
-	['red','red','blue','white']
-	['red','red','blue','white']
-	['red','red','blue','white']
-	['red','red','blue','white']
+	['red','red','blue','white'],
+	['red','red','blue','white'],
+	['red','red','blue','white'],
+	['red','red','blue','white'],
 ]
 
-io.on('connection', (ws) => {
-	ws.emit('pixel-data', pixelData)
+var clients = []
 
-	ws.on('disconnect', () => {
+io.on('connection', (socket) => {
+	clients.push(socket)
+
+	socket.emit('pixel-data', pixelData)
+
+	socket.on('draw-dot', ({row, col, color}) => {
+		pixelData[row][col] = color
+		socket.emit('update-dot', {row,col,color})
+		// clients.forEach(client => {
+		// 	client.emit('update-dot', {row, col, color})
+		// })
+	})
+	socket.on('disconnect', () => {
+		clients = clients.filter(it => it != socket)
 		console.log('son leaves')
 	})
 })
